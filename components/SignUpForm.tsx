2ad0d9f98649b63b80565e90914b57a3f501d249
Aidan Torrence <aidan.torrence@gmail.com>
2026-03-20 14:52:36 +0800

    AI: Change success message to thanks for signing up

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -152,7 +152,7 @@ export default function SignUpForm() {
               <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
             </svg>
           </div>
-          <p className="text-lg font-semibold text-white">You&apos;re in!</p>
+          <p className="text-lg font-semibold text-white">Thanks for signing up!</p>
           <p className="mt-1 text-sm text-white/60">I&apos;ll reach out soon to plan everything.</p>
         </div>
 
